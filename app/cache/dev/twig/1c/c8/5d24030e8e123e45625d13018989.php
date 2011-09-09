92071ed1693632213306290b52fca950c42daada
Jorge Martins <jorgemfm@gmail.com>
2011-09-09 19:16:12 +0100

    commit inicial

---
+++ app/cache/dev/twig/1c/c8/5d24030e8e123e45625d13018989.php
@@ -0,0 +1,26 @@
+<?php
+
+/* BloggerBlogBundle:Default:index.html.twig */
+class __TwigTemplate_1cc85d24030e8e123e45625d13018989 extends Twig_Template
+{
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $context = array_merge($this->env->getGlobals(), $context);
+
+        // line 1
+        echo "Hello ";
+        echo twig_escape_filter($this->env, $this->getContext($context, 'name'), "html");
+        echo "!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "BloggerBlogBundle:Default:index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+}
